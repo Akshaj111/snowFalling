c1c5ed0cf51d2bf8e328efd160bd8fa63e60ea3e
Akshaj111 <76700912+Akshaj111@users.noreply.github.com>
2021-05-02 16:49:33 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var bg = "snow2.jpg";
+function setup() {
+  createCanvas(800,400);
+  bg = createSprite(400,200,800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  bg.display();
+  drawSprites();
+}
